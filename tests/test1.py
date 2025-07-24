c795920e0b8ca4461a586b73e61b32e07afe7813
deadcube04 <gabrielmca2004@gmail.com>
2025-07-24 18:54:42 -0300

    SA working

--- tests/test1.py
+++ tests/test1.py
@@ -1,7 +1,25 @@
-test = {
+best = {
+        "jobs" : {
+            "job_1" : [([1, 2],[],1)],
+            "job_2" : [([1, 2],[],1)],
+        },
+        "machine_downtimes" : {},
+        "timespan": 2
+        }
+
+medium =  {
         "jobs" : {
             "job_1" : [([1],[],1)],
             "job_2" : [([2],[],1)]
+        },
+        "machine_downtimes" : {},
+        "timespan": 2
+        }
+
+worst =  {
+        "jobs" : {
+            "job_1" : [([1],[],1)],
+            "job_2" : [([1],[],1)]
         },
         "machine_downtimes" : {},
         "timespan": 2
