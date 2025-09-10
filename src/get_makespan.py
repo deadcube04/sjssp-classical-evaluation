6e0041b54b2f4965e9cb29284aa9d1738d9664af
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2025-09-10 16:42:02 -0300

    fixing major error at fitnnes function

--- src/get_makespan.py
+++ src/get_makespan.py
@@ -91,21 +91,17 @@ def solve_jssp(instance_json):
 # Exemplo de uso com seu JSON:
 if __name__ == "__main__":
     json_data = {
-        "jobs": {
-            "job_1": [([1, 3], [], 1)],
-            "job_2": [([2, 3], [], 1)],
-            "job_3": [([2, 3], [], 1)],
-            "job_4": [([1, 2, 3], [], 2)],
-            "job_5": [([1], [], 3)],
-            "job_6": [([1], [], 2)],
-            "job_7": [([1], [], 4)],
-            "job_8": [([1], [], 1)],
-            "job_9": [([2], [], 10)],
-            "job_10": [([3], [], 1)],
+        "jobs" : {
+            "job_1" : [([1, 3],[],1), ([1,3],[],1)],
+            "job_2" : [([2, 3],[],1), ([2,3],[],1)],
+            "job_3" : [([2, 3],[],1), ([2,3],[],1)],
+            "job_4" : [([1, 2, 3],[],2), ([1,2,3],[],2)],
+            "job_5" : [([1],[],3), ([1],[],3)],
+       
         },
-        "machine_downtimes": {},
-        "timespan": 20,  # um horizonte suficientemente grande
-    }
+        "machine_downtimes" : {},
+        "timespan": 6
+        }
 
     makespan = solve_jssp(json_data)
     print(f"Makespan mínimo calculado: {makespan}")
